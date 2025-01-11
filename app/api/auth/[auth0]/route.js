b1fd9de0d44e05cd3adce63e9029315879a8eef4
joeulam <joeulam345@gmail.com>
2025-01-11 17:59:59 -0500

    Fixed auth0 not redirecting to homepage

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,6 +1,8 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth,handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = await handleAuth({ signup: handleLogin({ authorizationParams: { screen_hint: 'signup' } })
+export const GET = await handleAuth({ 
+    login: handleLogin({returnTo: "/homepage"}),
+    signup: handleLogin({ authorizationParams: { screen_hint: 'signup' } })
 });
 
