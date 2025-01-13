52ba26fa141cfc1340632bf2626e198b0563a83b
joeulam <joeulam345@gmail.com>
2025-01-13 17:18:18 -0500

    Added list of transactions to transaction page

--- app/transaction/page.tsx
+++ app/transaction/page.tsx
@@ -5,21 +5,35 @@ import { monthlySpending, Transaction } from "../functions/transaction_functions
 import { useEffect, useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import dayjs from "dayjs";
-import { Skeleton } from "antd";
+import { Card, List, Skeleton } from "antd";
 import '@mantine/core/styles/global.css';
 import "@mantine/charts/styles.css";
 
+export function sortByDate(order: string, historicalTransaction: Transaction[]){
+  if(order === "new_to_old"){
+    const sortedTransactions = historicalTransaction.sort(
+      (a, b) => a.date!.valueOf() - b.date!.valueOf()
+    );
+    return sortedTransactions;
+  }
+}
+
 export default function TransactionPage() {
   const { user, isLoading, } = useUser();
-  const [historicalTransaction, setHistoricalTransaction] = useState<{date: string; "Total spending": number}[]>([]);
+  const [historicalSpending, sethistoricalSpending] = useState<{date: string; "Total spending": number}[]>([]);
+  const [historicalTransaction, setHistoricalTransaction] = useState<Transaction[]>([]);
   const [loading, setLoading] = useState(true);
 
   async function getData(){
     if(!isLoading && user){
       const userData = await monthlySpending(user!)
+      setHistoricalTransaction(userData.response.transactions)
       prepareChartData(userData.response.transactions)
     }
   }
+
+  
+
   function prepareChartData(transactions: Transaction[]) {
     // Sort transactions by date (ascending)
     const sortedTransactions = [...transactions].sort(
@@ -33,13 +47,14 @@ export default function TransactionPage() {
         "Total spending": cumulativeSum, // Cumulative sum up to this point
       };
     });
-    setHistoricalTransaction(chartData);
+    sethistoricalSpending(chartData);
     setLoading(false)
   }
 
   useEffect(() => {
     if (!isLoading && user) {
       getData();
+      setHistoricalTransaction(sortByDate("new_to_old", historicalTransaction)!);
     }
   }, [isLoading, user]); 
 
@@ -55,10 +70,12 @@ export default function TransactionPage() {
           </div>
         </Skeleton.Node>
       ) : (
+        <>
+        
         <LineChart
           style={{ width: "50%" }} 
           h={300}
-          data={historicalTransaction}
+          data={historicalSpending}
           dataKey="date"
           series={[
             { name: "Total spending", color: "indigo.6" },
@@ -66,6 +83,35 @@ export default function TransactionPage() {
           curveType="linear"
           connectNulls
         />
+        
+        
+        <List
+          itemLayout="horizontal"
+          dataSource={historicalTransaction}
+          renderItem={(item) => (
+            <Card>
+              <List.Item
+                actions={[
+                  <a
+                    // onClick={() => editTransaction(item)}
+                    // onKeyDown={() => editTransaction(item)}
+                    key="list-loadmore-edit"
+                  >
+                    edit
+                  </a>,
+                ]} // Make it popup modal and edit from there
+              >
+                <List.Item.Meta
+                  title={item.name} // Change so it dynamically updates with transaction
+                  description={"$" + item.cost} // Change so it dynamically updates with transaction
+                />
+              </List.Item>
+            </Card>
+          )}
+        />
+        </>
+        
+        
       )}
     </MantineProvider>
   )
