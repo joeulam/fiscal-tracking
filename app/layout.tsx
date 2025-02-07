68cdce477ad017b49d21984eb0db62eb53ce7b57
Joey Lam <56980755+joeulam@users.noreply.github.com>
2025-02-07 17:00:08 -0500

    Added analytics + only get monthly graph (#30)
    * Added analytics + only get monthly graph
    
    * Fixed black loading screen bug
    
    ---------
    
    Co-authored-by: joeulam <joeulam@bu.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,19 @@ export const metadata: Metadata = {
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { LayoutProps } from "@/.next/types/app/layout";
 
-
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-    <UserProvider>
-      <body>{children}</body>
-    </UserProvider>
+      <UserProvider>
+        <body>
+          {children}
+          <script
+            defer
+            src="https://cloud.umami.is/script.js"
+            data-website-id="e1c2962b-cf6a-4027-8640-4844c735afa2"
+          ></script>
+        </body>
+      </UserProvider>
     </html>
   );
 }
